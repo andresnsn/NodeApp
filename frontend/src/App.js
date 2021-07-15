5d74be10d4bdda5163c3587b04c799e2ce7117e7
André Ribeiro <andre.sn@live.com>
2021-07-15 01:50:09 -0300

    Iniciando o React

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import AboutUs from './pages/AboutUs'
+
+function App() {
+  return (
+    <div>
+      <AboutUs/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/AboutSection.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AboutSection = () => {
+    return(
+        <div>
+            <h2>André Ribeiro</h2>
+            <h2>Analista de sistemas</h2>
+            <p>Olá, meu nome é André! Sou analista de sistemas na GFT!</p>
+            <button>Contact me</button>
+            <img src="https://lh3.googleusercontent.com/a-/AOh14Giz7Lyy-cbzeppDx2SqVvHCpV6tSq5TSusWgn8e3w=s288-p-no"></img>
+        </div>
+
+
+    )
+
+}
+
+export default AboutSection
