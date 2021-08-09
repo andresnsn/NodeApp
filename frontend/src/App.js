736cd825cf3c8c44db4d4aaf2d86df9640aac01c
André Ribeiro <andre.sn@live.com>
2021-08-08 23:58:32 -0300

    Iniciando a área a administrativa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 //Global Style
 import GlobalStyle from './components/GlobalStyle';
 //Roteamento
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, useLocation } from 'react-router-dom'
 //Components
 import AboutUs from './pages/AboutUs'
 import Portfolio from './pages/Portfolio'
@@ -12,25 +12,22 @@ import Nav from './components/Nav'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import Admin from './pages/Admin'
 
 
 function App() {
+  const location = useLocation()
   library.add(fab, fas)
 
   return (
     <div>
       <GlobalStyle/>
-      <Nav/>
+      {location.pathname !== '/admin' && <Nav/>}
       <Switch>
-        <Route exact path = "/">
-          <AboutUs/>
-        </Route>
-        <Route path = {['/portfolio:id', '/portfolio']}>
-          <Portfolio/>
-        </Route>
-        <Route path = "/contact">
-          <ContactMe/>
-        </Route>
+        <Route exact path = "/" component={AboutUs}/>
+        <Route path = {['/portfolio:id', '/portfolio']} component={Portfolio}/>
+        <Route path = "/contact" component={ContactMe}/>
+        <Route path = '/admin' component={Admin}/>
       </Switch>
     </div>
   );
---
+++ frontend/src/pages/Admin.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Jumbotron, Tab, Tabs, Container} from 'react-bootstrap'
+import PortfolioList from '../components/admin/PortfolioList'
+
+const Admin = () => {
+    return(
+    <Container fluid>
+        <Jumbotron>
+            <h1>Admin Panel</h1>
+        </Jumbotron>
+        <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-3">
+            <Tab eventKey="home" title="Home">
+                <h2>Welcome</h2>
+            </Tab>
+            <Tab eventKey="portfolio" title="Portfolio">
+                <PortfolioList/>
+            </Tab>
+            <Tab eventKey="tech" title="Technologies">
+                <h2>Technologies</h2>
+            </Tab>
+        </Tabs>
+    </Container>
+    )
+}
+
+export default Admin
